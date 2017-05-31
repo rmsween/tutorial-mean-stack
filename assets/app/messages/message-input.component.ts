dcd6ec9648eba7adbe3cde39099e2bb85e602dd3
Sweeney <Sweeney@SweeneysMacBook.lan>
2017-05-31 13:20:20 -0400

    Connecting Angular2 Frontend with the NodeJS Backend

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,17 +1,44 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {MessageService} from "./message.service";
 import {Message} from "./message.model";
 import {NgForm} from "@angular/forms";
+
 @Component({
     selector: 'app-message-input',
     templateUrl: './message-input.component.html'
 })
-export class MessageInputComponent {
+export class MessageInputComponent implements OnInit {
+    message: Message;
+
     constructor(private messageService: MessageService) {}
 
     onSubmit(form: NgForm) {
-        const message = new Message(form.value.content, 'Max');
-        this.messageService.addMessage(message);
+        if (this.message) {
+            this.message.content = form.value.content;
+            this.messageService.updateMessage(this.message)
+                .subscribe(
+                    result => console.log(result)
+                );
+            this.message = null;
+        } else {
+            const message = new Message(form.value.content, 'Max');
+            this.messageService.addMessage(message)
+                .subscribe(
+                    data => console.log(data),
+                    error => console.error(error)
+                );
+        }
         form.resetForm();
+    }
+
+    onClear(form) {
+        this.message = null;
+        form.resetForm()
+    }
+
+    ngOnInit() {
+        this.messageService.messageIsEdit.subscribe(
+            (message: Message) => this.message = message;
+        );
     }
 }
